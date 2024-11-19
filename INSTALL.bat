5fb821abfa16a70ece524bc1e986dd72c01b1a72
code-switched <switched.code@gmail.com>
2024-11-18 23:21:49 -0600

    no need to activate

--- INSTALL.bat
+++ INSTALL.bat
@@ -3,10 +3,9 @@ git clone https://github.com/code-switched/whisper_captions.git
 cd .\whisper_captions\
 py -3.10 -m venv --prompt "whcap" venv
 .\venv\Scripts\python -m pip install --upgrade pip wheel setuptools
-.\venv\Scripts\activate
-pip install torch torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install librosa soundfile faster-whisper pyaudio
-pip install git+https://github.com/linto-ai/whisper-timestamped
+.\venv\Scripts\python -m pip install torch torchaudio --index-url https://download.pytorch.org/whl/cu118
+.\venv\Scripts\python -m pip install librosa soundfile faster-whisper pyaudio
+.\venv\Scripts\python -m pip install git+https://github.com/linto-ai/whisper-timestamped
 
 :: Create shortcut with proper command prompt settings
 $WS = New-Object -ComObject WScript.Shell; $SC = $WS.CreateShortcut("$env:USERPROFILE\Desktop\LIVE Captions Server.lnk"); $SC.TargetPath = "$PWD\server-connect-captions.bat"; $SC.WorkingDirectory = "$PWD"; $SC.Save()
